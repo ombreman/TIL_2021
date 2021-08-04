2ec5e15bac5d735d6e7638c808e2743ce82087eb
ombreman <ombreman21@gmail.com>
2021-08-04 13:23:34 +0100

    ts prac

--- Prac_typescript/app.js
+++ Prac_typescript/app.js
@@ -1,2 +1,19 @@
 "use strict";
-var a = 5;
+var studentID = 12345;
+var studentName = 'Changgil Jeong';
+var age = 31;
+var gender = 'male';
+var subject = 'Javascript';
+var courseCompleted = false;
+function getStudentDetails(studentID) {
+    return {
+        studentID: 123456,
+        studentName: 'Jack',
+        gender: 'male',
+        subject: 'Node Js',
+        courseCompleted: true
+    };
+}
+function saveStudentDetails(student) {
+}
+saveStudentDetails({});
--- Prac_typescript/app.ts
+++ Prac_typescript/app.ts
@@ -1,1 +1,35 @@
-let a = 5
+let studentID: number = 12345
+let studentName: string = 'Changgil Jeong'
+let age: number = 31
+let gender: string = 'male'
+let subject: string = 'Javascript'
+let courseCompleted: boolean = false
+
+interface Student {
+  studentID: number
+  studentName: string
+  age?: number
+  gender: string
+  subject: string
+  courseCompleted: boolean
+  addComment?: (comment: string) => string 
+}
+
+function getStudentDetails(studentID: number): Student
+{
+  return {
+    studentID: 123456,
+    studentName: 'Jack',
+    gender: 'male',
+    subject: 'Node Js',
+    courseCompleted: true 
+  }
+}
+
+function saveStudentDetails (student: Student): void {
+
+}
+
+saveStudentDetails ({
+  
+})
