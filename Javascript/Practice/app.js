594c00b49f98c2aff41e20e836a181629a0b2b25
ombreman <ombreman21@gmail.com>
2021-08-22 13:47:06 +0100

    docs: add new words

--- Javascript/Practice/app.js
+++ Javascript/Practice/app.js
@@ -1,18 +1,7 @@
-//Object
-
-const player = {
-  name: "Changgil",
-  points: 10,
-  fat: true
+// Function
+function sayHello(anyname) {
+  console.log("Hello my name is " + anyname)
 }
 
-console.log(player)
-console.log(player["name"])
-console.log(player.name)
-console.log(player.points)
-console.log(player.fat)
-player.fat = false
-console.log(player.fat)
-
-player.gender = "male"
-console.log(player)
+sayHello("창길")
+alert("Hey")
--- Subjects/new words.md
+++ Subjects/new words.md
@@ -5,3 +5,9 @@
 
 ### Vocabulary
 1. terser: really really simple
+
+### Abbreviation
+1. NaN: Not a number
+2. OOP: Object Oriented Programming
+3. TDD: Test Driven Development
+4. 
