b916072992f7018f31f791ac2ee5ed49692a633a
ombreman <ombreman21@gmail.com>
2021-08-27 23:30:36 +0100

    Feat: className JS prac

--- Javascript/Practice/app.js
+++ Javascript/Practice/app.js
@@ -1,14 +1,12 @@
 const h1 = document.querySelector("div.hello:first-child h1")
 
 function handleTitleClick() {
-  const currentColor = h1.style.color
-  let newColor
-  if (currentColor === "blue") {
-    newColor = "tomato"
+  // h1.className = "active"
+  if (h1.className === "active") {
+    h1.className = ""
   } else {
-    newColor = "blue"
+    h1.className = "active"
   }
-  h1.style.color = newColor
 }
 
 h1.addEventListener("click", handleTitleClick)
