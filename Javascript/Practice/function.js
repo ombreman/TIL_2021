c289b9c23c607269a4a1b2c833b44bd56997b73e
ombreman <ombreman21@gmail.com>
2021-09-19 11:27:01 +0100

    Add JS class study

--- Javascript/Practice/function.js
+++ Javascript/Practice/function.js
@@ -146,3 +146,25 @@ const simpleMultiply = (a, b) => { // if you have to use brakets
 (function hello() {
   console.log('IIFE');
 })(); // IIFE
+
+// Fun quiz time
+// function calculate(command, a, b)
+// command: add, substract, divide, multiply, remainder
+
+function calculate(command, a, b) {
+  switch (command) {
+    case 'add':
+      return a + b;
+    case 'substract':
+      return a - b;
+    case 'divide':
+      return a / b;
+    case 'multiply':
+      return a * b;
+    case 'remainder':
+      return a % b;
+    default:
+      throw Error('unknown command');
+  }
+}
+console.log(calculate('add', 2, 3));
