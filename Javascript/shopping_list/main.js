e5e12d3628d40cffd5c765813327a81e128a67ac
ombreman <ombreman21@gmail.com>
2021-10-07 23:48:26 +0100

    Add event bubbling and capturing

--- Javascript/shopping_list/main.js
+++ Javascript/shopping_list/main.js
@@ -25,32 +25,40 @@ function onAdd() {
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
+  itemRow.innerHTML = `
+      <div class="item">
+        <span class="item__name">Egg</span>
+        <button class="item__delete">
+          <i class="fas fa-trash-alt"></i>
+        </button>
+      </div>
+      <div class="item__divider"></div>`
 
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
+  // const item = document.createElement('div');
+  // item.setAttribute('class', 'item');
 
-  const name = document.createElement('span');
-  name.setAttribute('class', 'item__name');
-  name.innerText = text;
-
-
+  // const name = document.createElement('span');
+  // name.setAttribute('class', 'item__name');
+  // name.innerText = text;
 
 
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'item__delete');
-  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>'
-  deleteBtn.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  })
 
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
 
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
+  // const deleteBtn = document.createElement('button');
+  // deleteBtn.setAttribute('class', 'item__delete');
+  // deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>'
+  // deleteBtn.addEventListener('click', () => {
+  //   items.removeChild(itemRow);
+  // })
 
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  // const itemDivider = document.createElement('div');
+  // itemDivider.setAttribute('class', 'item__divider');
+
+  // item.appendChild(name);
+  // item.appendChild(deleteBtn);
+
+  // itemRow.appendChild(item);
+  // itemRow.appendChild(itemDivider);
 
   return itemRow;
 }
