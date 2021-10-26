b84294843dadd3fe56f3502b587e995897b95619
ombreman <ombreman21@gmail.com>
2021-10-26 17:23:13 +0100

    Finish Vanila JS login practice

--- Javascript/Practice/login/app.js
+++ Javascript/Practice/login/app.js
@@ -8,23 +8,21 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const USERNAME_KEY = "username"
+const USERNAME_KEY = "username";
 const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username)
+  localStorage.setItem(USERNAME_KEY, username); // saving username in localstorage
+  paintGreetings(username);
 };
-
-loginForm.addEventListener("submit", onLoginSubmit);
 
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
-}
+};
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
@@ -32,5 +30,5 @@ if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  paintGreetings(savedUsername)
-}
+  paintGreetings(savedUsername);
+};
